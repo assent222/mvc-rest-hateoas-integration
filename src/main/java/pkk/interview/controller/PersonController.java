779e86590fd72110c5e9a3d123bd77dfefd75490
pkk <assent222@gmail.com>
2017-01-26 21:40:44 +0200

    add HandlerMethodArgument

--- src/main/java/pkk/interview/controller/PersonController.java
+++ src/main/java/pkk/interview/controller/PersonController.java
@@ -6,12 +6,12 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import pkk.interview.domain.Person;
+import pkk.interview.interceptor.HateoasResourceAssembler;
 import pkk.interview.service.PersonService;
 
 import java.util.ArrayList;
@@ -23,28 +23,37 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
  * Created by root on 26.01.2017.
  */
 
-@RestController
+@Controller
 @RequestMapping(path = "/rest")
 public class PersonController {
 
     @Autowired
     private PersonService service;
 
-    @RequestMapping(path = "/persons")
-    public ResponseEntity persons() {
+    @RequestMapping(path = "/persons", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+
+    public ResponseEntity persons(HateoasResourceAssembler resourceAssembler) {
+        System.out.println(">> persons");
+
+
         Link link = linkTo(this.getClass()).slash("/persons").withSelfRel();
 
 
         List<Resource> list = new ArrayList<>();
         for (Person person : service.findAll()) {
             list.add(new Resource(person, linkTo(this.getClass()).slash("/persons/" + person.getId()).withSelfRel()));
+            System.out.println("resourceAssembler=" + resourceAssembler.toResource(person));
         }
 
         Resources<Resource> resources = new Resources<Resource>(list,link);
+
+        System.out.println("<< persons");
         return new ResponseEntity(resources, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/persons/{id}")
+    @ResponseBody
     public Object findPerson(@PathVariable Integer id) {
         Person person = service.find(id);
         Link link = linkTo(this.getClass()).slash(id).withSelfRel();
