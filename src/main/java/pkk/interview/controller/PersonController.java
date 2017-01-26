3f064affd358e0bd6140f252870d95ca600ce6e0
pkk <assent222@gmail.com>
2017-01-26 16:12:48 +0200

    update response type in controller

--- src/main/java/pkk/interview/controller/PersonController.java
+++ src/main/java/pkk/interview/controller/PersonController.java
@@ -1,14 +1,21 @@
 package pkk.interview.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pkk.interview.domain.Person;
 import pkk.interview.service.PersonService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
@@ -24,8 +31,17 @@ public class PersonController {
     private PersonService service;
 
     @RequestMapping(path = "/persons")
-    public Object persons() {
-        return service.findAll();
+    public ResponseEntity persons() {
+        Link link = linkTo(this.getClass()).slash("/persons").withSelfRel();
+
+
+        List<Resource> list = new ArrayList<>();
+        for (Person person : service.findAll()) {
+            list.add(new Resource(person, linkTo(this.getClass()).slash("/persons/" + person.getId()).withSelfRel()));
+        }
+
+        Resources<Resource> resources = new Resources<Resource>(list,link);
+        return new ResponseEntity(resources, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/persons/{id}")
